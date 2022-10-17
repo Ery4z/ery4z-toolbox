4b80722cb5c9794e2609bfaa7939ceaa860bafa9
Thomas Bolteau <48215561+Ery4z@users.noreply.github.com>
2022-10-17 13:12:02 +0200

    Update setup_zabbix_agent.sh

--- scripts/setup_zabbix_agent.sh
+++ scripts/setup_zabbix_agent.sh
@@ -3,6 +3,7 @@ sudo dpkg -i zabbix-release_6.2-2+ubuntu18.04_all.deb
 sudo apt update
 sudo apt install zabbix-agent
 sudo cp zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
+sudo mkdir /etc/zabbix/zabbix_agentd.conf.d
 sudo systemctl restart zabbix-agent
 
 echo "Please modify the hostname in /etc/zabbix/zabbix_agentd.conf"
