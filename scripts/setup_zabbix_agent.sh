77a0c25120521da3f2711e5d194ea3b3d15c05a0
Thomas Bolteau <48215561+Ery4z@users.noreply.github.com>
2022-10-17 14:33:15 +0200

    Update setup_zabbix_agent.sh

--- scripts/setup_zabbix_agent.sh
+++ scripts/setup_zabbix_agent.sh
@@ -1,5 +1,5 @@
 echo "Please enter the name of the host:"
-read -p $HOSTNAME
+read -p $HOSTNAMEZABBIX
 
 wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-2%2Bubuntu18.04_all.deb
 sudo dpkg -i zabbix-release_6.2-2+ubuntu18.04_all.deb
@@ -9,7 +9,7 @@ sudo apt update
 
 sudo apt install zabbix-agent
 
-sed -i 's|HOSTNAMEPLACEHOLDER|'"$HOSTNAME"'|g' zabbix_agentd.conf
+sed -i 's|HOSTNAMEPLACEHOLDER|'"$HOSTNAMEZABBIX"'|g' zabbix_agentd.conf
 sudo cp zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
 sudo mkdir /etc/zabbix/zabbix_agentd.conf.d
 sudo systemctl restart zabbix-agent
