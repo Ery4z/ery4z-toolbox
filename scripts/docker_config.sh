d817ea077a17821f5d1b328183ca09629ddb6728
Thomas Bolteau <48215561+Ery4z@users.noreply.github.com>
2022-11-19 11:05:21 +0100

    Create docker_config.sh

---
+++ scripts/docker_config.sh
@@ -0,0 +1,12 @@
+sudo systemctl stop docker.service
+sudo systemctl stop docker.socket
+
+sudo mkdir -p /home/docker
+sudo sed  -e '/tc_/s/$/ -g /home/docker/'
+
+sudo rsync -aqxP /var/lib/docker/ /home/docker
+
+sudo systemctl daemon-reload
+sudo systemctl start docker
+
+ps aux | grep -i docker | grep -v grep
