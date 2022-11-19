b17e1e5461d84e77c12f6b32de46e99f1b5db995
Thomas Bolteau <48215561+Ery4z@users.noreply.github.com>
2022-11-19 12:07:44 +0100

    Update setup_zabbix_agent.sh

--- scripts/setup_zabbix_agent.sh
+++ scripts/setup_zabbix_agent.sh
@@ -1,17 +1,19 @@
 echo "Please enter the name of the host:"
 read
 
-wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-2%2Bubuntu18.04_all.deb
-sudo dpkg -i zabbix-release_6.2-2+ubuntu18.04_all.deb
+wget https://repo.zabbix.com/zabbix/6.2/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.2-4%2Bubuntu20.04_all.deb
+sudo dpkg -i zabbix-release_6.2-4+ubuntu20.04_all.deb
 sudo apt update
 
 
 
 sudo apt install zabbix-agent
 cp zabbix_agentd.conf.original zabbix_agentd.conf
-sed -i 's|HOSTNAMEPLACEHOLDER|'"$REPLY"'|g' zabbix_agentd.conf
-sudo cp zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
-sudo mkdir /etc/zabbix/zabbix_agentd.conf.d
+sed -i 's|Zabbix Server|'"$REPLY"'|g' /etc/zabbix/zabbix_agentd.conf
+sed -i 's|127.0.0.1|45.92.110.208|g' /etc/zabbix/zabbix_agentd.conf
+
+sudo ufw allow 10050
+
 sudo systemctl restart zabbix-agent
 
 echo "Please modify the hostname in /etc/zabbix/zabbix_agentd.conf"
